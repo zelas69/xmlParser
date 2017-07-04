1109bb93a2d8128c1a984e8a255a6643e29fff00
zelas <zelas69@gmail.com>
2017-07-04 15:39:50 +0300

    Exception added.

--- xmlParser/xmlException.cpp
+++ xmlParser/xmlException.cpp
@@ -1,2 +1,17 @@
+#include "xmlException.h"
+
 namespace XMLPARSER {
+    cXMLexception::cXMLexception(const std::string &exception_text) throw() : exception_text_(exception_text) {}
+
+    cXMLexception::cXMLexception(const cXMLexception &exception) throw() : std::exception(exception), exception_text_(exception.exception_text_) {}
+
+    cXMLexception& cXMLexception::operator= (const cXMLexception &exception) throw() {
+        cXMLexception tmp(exception);
+        std::swap(*this, tmp);
+        return *this;
+    }
+
+    const char* cXMLexception::what() const throw() {
+        return exception_text_.c_str();
+    }
 }
--- xmlParser/xmlException.h
+++ xmlParser/xmlException.h
@@ -1,8 +1,19 @@
 #pragma once
 #ifndef XML_EXCEPTION_H
 #define XML_EXCEPTION_H
+#include <exception>
+#include <string>
 
 namespace XMLPARSER {
+    class cXMLexception : std::exception {
+        std::string exception_text_;
+    public:
+        cXMLexception(const std::string &exception_text) throw();
+        cXMLexception(const cXMLexception &exception) throw();
+        cXMLexception& operator= (const cXMLexception&) throw();
+        virtual ~cXMLexception() throw() {};
+        virtual const char* what() const throw();
+    };
 }
 
 #endif  XML_EXCEPTION_H
