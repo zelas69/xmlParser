0678fcd1dae473b75bc1a3a440c5e598857e3aa0
zelas <zelas69@gmail.com>
2017-07-04 10:58:16 +0300

    Add project files.

---
+++ xmlParser/xmlParser.cpp
@@ -0,0 +1,207 @@
+#include "xmlParser.h"
+#include <iostream>
+using std::cout;
+using std::cin;
+using std::vector;
+using std::string;
+using std::ifstream;
+using std::istream;
+
+namespace {
+    //enum class
+    enum {OPEN, CLOSE, SINGLE};
+}
+
+namespace XMLPARSER {
+    void get_lines_from_fstream(ifstream &stream, vector<string> &file_lines) {
+        string file_line;
+
+        while (!stream.eof()) {
+            getline(stream, file_line);
+            file_lines.push_back(file_line);
+        }
+    }
+
+    bool cTagStack::empty() const {
+        return tags_.empty();
+    }
+
+    void cTagStack::push(const std::string& tag) {
+        tags_.push_back(tag);
+    }
+
+    void cTagStack::pop() {
+        tags_.pop_back();
+    }
+
+    string cTagStack::extract_tag(const string &line) {
+        size_t tag_start_index = line.find_first_of(OPEN_BRACKET) + 1;
+        size_t tag_end_index = line.find_first_of(SPACE, tag_start_index);
+        
+        // ?????????????
+        if (tag_end_index == line.size()) {
+            tag_end_index == line.size() - 1;
+        }
+        string tag = line.substr(tag_start_index, tag_end_index - tag_start_index);
+        
+        if (line[line.find_last_of(CLOSE_BRACKET) - 1] == SLASH) {
+            tag = tag + SLASH;
+        }
+        return tag;
+    }
+
+    int cTagStack::check_tag(std::string &tag) const {
+        if (tag[0] == SLASH) {
+            tag = tag.substr(1);
+            return CLOSE;
+        }
+
+        if (tag[tag.size() - 1] == SLASH || tag[0] == '?' || tag[0] == '!') {
+            tag = tag.substr(0, tag.size() - 1);
+            return SINGLE;
+        }
+        return OPEN;
+    }
+
+    bool cTagStack::match_top(const std::string& tag) {
+        if (tags_.size() > 0 && tags_.back() == tag) {
+            pop();
+            return true;
+        }
+        return false;
+    }
+    
+    cXMLnode::~cXMLnode() {
+        for (size_t child_number = 0; child_number < children_.size(); ++child_number) {
+            delete children_[child_number];
+        }
+    }
+
+    cXMLnode::cXMLnode(std::string *data, cXMLnode *parent) : data_(data), parent_(parent) {}
+
+    size_t cXMLnode::get_number_of_children() const {
+        return children_.size();
+    }
+
+    cXMLnode* cXMLnode::get_parent() const {
+        return parent_;
+    }
+
+    std::vector<cXMLnode*>& cXMLnode::get_children() {
+        return children_;
+    }
+
+    cXMLnode* cXMLnode::get_child(const size_t &index) const {
+        return children_[index];
+    }
+
+    void cXMLnode::add_child(cXMLnode *child) {
+//        cout << *child->data_ << '\n';
+        children_.push_back(child);
+    }
+
+    //std::map<string, string> cXMLnode::extract_params() {}
+
+
+    cXMLtree::cXMLtree(std::vector<std::string>& file_lines) {
+        data_.swap(file_lines);
+        head_ = new cXMLnode();
+        current_ = head_;
+    }
+
+    cXMLtree::~cXMLtree() {
+        delete head_;
+    }
+
+    cXMLnode* cXMLtree::get_current() const {
+        return current_;
+    }
+
+    cXMLnode* cXMLtree::go_to_head() {
+        current_ = head_;
+        return current_;
+    }
+
+    cXMLnode* cXMLtree::go_to_child(const size_t &index) {
+        current_ = current_->get_child(index);
+        return current_;
+    }
+
+    cXMLnode* cXMLtree::go_to_parent() {
+        current_ = current_->get_parent();
+        return current_;
+    }
+
+    bool cXMLtree::build_tree() {
+        cTagStack tag_stack;
+
+        for (size_t line_number = 0; line_number < data_.size(); ++line_number) {
+            string tag = tag_stack.extract_tag(data_[line_number]);
+            int tag_type = tag_stack.check_tag(tag);
+            
+            if (tag_type == CLOSE) {
+                if (tag_stack.match_top(tag)) {
+                    go_to_parent();
+                }
+                else {
+                    //error!!!
+                    return false;
+                }
+            }
+            else {
+                cXMLnode *new_node = new cXMLnode(&data_[line_number], current_);
+                current_->add_child(new_node);
+
+                if (tag_type == OPEN) {
+                    tag_stack.push(tag);
+                    current_ = new_node;
+                }
+                tag_positions_[tag].push_back(current_);
+            }
+        }
+        current_ = head_;
+        return true;
+    }
+
+    //bool test_xml_tree(cXMLtree& xml_tree) {
+
+    //}
+
+    //std::string cXMLtree::extract_tag() {}                                          //?
+
+    //std::map<string, string> cXMLtree::extract_params() {}
+}
+
+int main() {
+    using namespace XMLPARSER;
+
+    const string path = "E:\\Work\\main\\grandsmeta_xml_smeta\\пример сметы для XML.xml";
+    ifstream infile(path);
+    vector<string> file_lines;
+    get_lines_from_fstream(infile, file_lines);
+
+    cXMLtree xml_tree(file_lines);
+    cout << xml_tree.build_tree() << '\n';
+
+    //for (size_t i = 0; i < file_lines.size(); ++i) {
+    //  cout << file_lines[i] << '\n';
+    //}
+
+    /*vector<cXMLnode> nodes;
+    cXMLnode node_1;
+    nodes.push_back(node_1);
+    
+
+    cXMLnode node_2;
+    nodes.push_back(node_2);
+    cXMLnode *node_ptr_2 = &nodes.back();
+
+    cXMLnode *node_ptr_1 = &nodes[0];
+
+    node_ptr_1->add_child(node_ptr_2);*/
+    
+
+    cout << "It works!\n";
+
+    return 0;
+}
---
+++ xmlParser/xmlParser.h
@@ -0,0 +1,92 @@
+#pragma once
+
+#ifndef XML_PARSER_H
+#define XML_PARSER_H
+
+#include <string>
+#include <vector>
+#include <fstream>
+#include <map>
+
+
+//typedef std::vector<std::string> VSTR;
+//using VSTR = std::vector<std::string>;
+typedef unsigned short ushort;
+
+namespace XMLPARSER {
+    void get_lines_from_fstream(std::ifstream &stream, std::vector<std::string> &file_lines);
+
+    class cTagStack {
+    private:
+        std::vector<std::string> tags_;
+        const char OPEN_BRACKET = '<';
+        const char CLOSE_BRACKET = '>';
+        const char SPACE = ' ';
+        const char SLASH = '/';
+
+    public:
+        bool empty() const;
+
+        void push(const std::string& tag);
+
+        void pop();
+
+        std::string extract_tag(const std::string &line);
+
+        int check_tag(std::string &tag) const;
+
+        bool match_top(const std::string &tag);
+    };
+
+
+    class cXMLnode {
+    private:
+        std::string *data_;
+        cXMLnode *parent_;
+        std::vector<cXMLnode*> children_;
+
+    public:
+        ~cXMLnode();
+
+        cXMLnode(std::string *data=NULL, cXMLnode *parent=NULL);
+
+        size_t get_number_of_children() const;
+
+        cXMLnode* get_parent() const;
+
+        std::vector<cXMLnode*>& get_children();
+
+        cXMLnode* get_child(const size_t &index) const;
+
+        void add_child(cXMLnode *child);
+
+        std::map<std::string, std::string> extract_params();
+    };
+
+
+    class cXMLtree {
+    private:
+        std::vector<std::string> data_;
+        std::map<std::string, std::vector<cXMLnode*> > tag_positions_;
+        cXMLnode *current_;
+        cXMLnode *head_;
+        
+        cXMLnode* get_current() const;
+
+        cXMLnode* go_to_head();
+
+        cXMLnode* go_to_child(const size_t &index);
+
+        cXMLnode* go_to_parent();
+
+    public:
+        cXMLtree(std::vector<std::string>& file_lines);   // swap
+
+        ~cXMLtree();
+
+        bool build_tree();
+
+    };
+}
+
+#endif // !XML_PARSER_H
